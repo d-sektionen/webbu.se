ed4244f3bfa0b6f04cc1281515ac1b085a60a175
LittleFoxCode <holtzjohan@gmail.com>
2022-09-15 20:16:12 +0200

    Finish base layout and look
    Co-authored-by: Therese Selberg <these002@student.liu.se>
    Co-authored-by: CajsaWargren <CajsaWargren@users.noreply.github.com>
    Co-authored-by: Thea <theaborg@users.noreply.github.com>
    Co-authored-by: Erik Ekelöf <Erik1001@users.noreply.github.com>
    Co-authored-by: Isak Horvath <tavro@users.noreply.github.com>

--- include-components.js
+++ include-components.js
@@ -1,22 +1,31 @@
+const getLoc=()=>{
+    const arr = window.location.href.split("/");
+    return arr[arr.length - 1].split(".")[0];
+}
+
+const getHeaderLogo = ()=>{
+    const logoImg= document.createElement("img");
+    logoImg.src = "webbu-logo-inverted.png";
+    const logoA = document.createElement("a");
+    logoA.href = "index.html";
+    logoA.appendChild(logoImg);
+    const header = document.createElement("header");
+    header.appendChild(logoA);
+    return header;
+};
+
 const getNavA= (text, href, selected)=>{
     const nav = document.createElement("a");
     nav.innerText = text;
     nav.href = href;
     nav.className = selected ? "selected" : "";
     return nav;
-}
+};
 
 const getHeader = ()=>{
-    const logoImg= document.createElement("img");
-    logoImg.src = "webbu-logo.png";
-    const logoA = document.createElement("a");
-    logoA.href = "/";
-    logoA.appendChild(logoImg);
-    
-    const arr = window.location.href.split("/");
-    const loc = arr[arr.length - 1].split(".")[0];
+    const loc =getLoc();
 
-    const navA1 = getNavA("Om oss",".", loc=="");
+    const navA1 = getNavA("Om oss","index.html", loc=="index" || loc=="");
     const navA2 = getNavA("Tjänster","services.html", loc=="services");
     const navA3 = getNavA("Hackathon","hackathon.html", loc=="hackathon");
     const navA4 = getNavA("Kontakt","contact.html", loc=="contact");
@@ -27,11 +36,10 @@ const getHeader = ()=>{
     nav.appendChild(navA3);
     nav.appendChild(navA4);
 
-    const header = document.createElement("header");
-    header.appendChild(logoA);
-    header.appendChild(nav);
-    
-    return header;
+    const navBar = document.createElement("div");
+    navBar.appendChild(nav);
+    navBar.id = "navBar";
+    return navBar;
 };
 
 const getFooter = ()=>{
@@ -50,4 +58,36 @@ const getFooter = ()=>{
 };
 
 document.body.prepend(getHeader());
+document.body.prepend(getHeaderLogo());
 document.body.appendChild(getFooter());
+
+
+const getContactBox= (c)=>{
+    const div = document.createElement("a");
+
+//     <div class="contact-card">
+//     <img src="johndoe.png">
+//     <div>
+//     <h2>
+//         Isak Horvath </br> <span class="position">Webmaster</span>
+//     </h2>
+//     <p>
+//         webmaster@d-sektionen.se
+//     </p>
+//     <!--“kontakta mig vid allmänna frågor om webbutskottet och dess verksamhet”-->
+//     </div>
+// </div>
+    div.className = "contact-card";
+
+
+    return div;
+};
+const fillContacts = ()=>{
+    if(getLoc() != "contact")
+        return;
+    const cs = [];
+    for (const c of cs) {
+        getContactBox(c);
+    }
+};
+fillContacts();
