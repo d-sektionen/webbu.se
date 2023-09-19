4e0c226234ada65bb47f4138d7cbe20b48ea0c32
Therese Selberg <these002@student.liu.se>
2023-09-19 20:46:29 +0200

    jobbar på att fixa storbild när en av bilderna i wall of fame klickas, centrering...

--- files/walloffame.js
+++ files/walloffame.js
@@ -11,13 +11,47 @@ const getYearPost = c => {
     
     div.appendChild(img);
     div.appendChild(h1);
-    // div.onclick(openBigImage());
+    // Onclick function for the button that opens the larger image
+    div.onclick = function() {
+        const wrapper = document.getElementById("image-wrapper");
+        const modal = document.getElementById("enlarged-image-window");
+        wrapper.style.display = "block";
+        modal.style.margin = "1em auto auto auto"
+        const image = document.getElementById("enlarged-image");
+        image.src = c.img;
+    }
+
+    getOffset(img);
     return div;
 };
 
-// const openBigImage = async () => {
 
-// }
+const getOffset = image => {
+    //behöver anpassa efter om den är bredare än skärmen eller högre än skärmen så att det blir dynamiskt 
+    const winHeight = window.screen.availHeight;
+    const winWidth = window.screen.availWidth;
+    const imgWidth = image.width;
+    const imgHeight = image.height;
+    var relH = winHeight/imgHeight;
+
+    console.log(winHeight);
+    console.log(winWidth);
+    console.log(imgHeight);
+    console.log(imgWidth);
+    console.log(relH);
+
+}
+
+const bigImage = async () => {
+  // Onclick function to close the modal when clicking outside
+  const wrapper = document.getElementById("image-wrapper");
+  wrapper.onclick = function(event) {
+    const wrapper = document.getElementById("image-wrapper");
+    if (event.target == wrapper) {
+      wrapper.style.display = "none";
+    }
+  }
+}
 
 const makeWall = async () => {
     if (getLoc() != "walloffame") 
@@ -31,75 +65,4 @@ const makeWall = async () => {
 };
 
 makeWall();
-
-
-/* D-groups onlcick grej i fo*/
-
-
-/*export default function CardGrid({ user }) {
-    const [open, setOpen] = useState(false);
-    const handleClick = () => {
-      setOpen(!open);
-    };
-    return (
-      <Box
-        onClick={handleClick}
-        sx={{
-          borderRadius: 20 + "px",
-          cursor: "pointer",
-          boxShadow: "2px 2px 10px #D3D3D3",
-        }}
-      >
-        <Grid container sx={{width: '100%'}}>
-          <ThemeProvider theme={questionTheme}>
-            <Grid xs={5}>
-              <SmallP src={user.smallImage} />
-              <BigP src={user.image} />
-            </Grid>
-            <Grid xs={7} sx={{ py: '2em', px: '1em' }}>
-              <Typography variant="h5" guttomButtom>
-                {user.name}
-              </Typography>
-              <Typography variant="h6" guttomButtom>
-                {user.post}
-              </Typography>
-              <Typography variant="h6" guttomBottom>
-                <a href={"mailto:" + user.email}>{user.email}</a>
-              </Typography>
-              <DGLogo src={dg_logo}></DGLogo>
-            </Grid>
-          </ThemeProvider>
-        </Grid>
-  
-        <Modal open={open} onClose={handleClick}>
-          <Box sx={style}>
-            <Grid container>
-              <Grid xs={12}>
-                <img
-                  src={user.image}
-                  alt="Gropies"
-                  height="300vh"
-                  width="300vh"
-                  style={{
-                    marginTop: 10,
-                    marginLeft: 25,
-                    objectFit: "cover",
-                    borderRadius: 100 + "%",
-                  }}
-                />
-              </Grid>
-  
-              <Grid xs={12}>
-                <Questions user={user} />
-              </Grid>
-              <Grid>
-                <Button variant="contained" onclick={handleClick} sx={{ ml: 17 }}>
-                  close
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
-        </Modal>
-      </Box>
-    );*/
-
+bigImage();
